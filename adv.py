9c7e6ca45e802201519dbb954f07a75c9d93b2a3
Adam Payne <ajpayne238@gmail.com>
2020-03-20 13:04:53 -0400

    traversing all rooms. removed queue

--- adv.py
+++ adv.py
@@ -41,10 +41,50 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
+print("THIS", player.current_room.id)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+backwards_path = []
+
+# Add backwards directions to get out of rooms with no exits by going back to previous rooms
+backwards_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+# Create visited dictionary
+visited = {}
+
+# Start in room 0 with current exits
+visited[0] = player.current_room.get_exits()
+
+# Loop while we have not visited all rooms in room_graph
+while len(visited) < len(room_graph):
+    # Check if current room not in visited
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = player.current_room.get_exits()
+        last_path = backwards_path[-1]
+        visited[player.current_room.id].remove(last_path)
+    # Loop backwards once we reach a room with no directions
+    while len(visited[player.current_room.id]) < 1:
+        # Pop the back to last set of get exits and append it to traversal path
+        backwards = backwards_path.pop()
+        traversal_path.append(backwards)
+        # Now we travel backwards
+        player.travel(backwards)
+    # Pop current room from visited
+    exit_room = visited[player.current_room.id].pop(0)
+    # Append it to traversal path
+    traversal_path.append(exit_room)
+    backwards_path.append(backwards_directions[exit_room])
+    player.travel(exit_room)
+
+    print(len(room_graph) - len(visited))
+    if len(room_graph) - len(visited) == 1:
+        visited[player.current_room.id] = player.current_room.get_exits()
+
+
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -64,6 +104,7 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
+"""
 player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
@@ -73,3 +114,4 @@ while True:
         break
     else:
         print("I did not understand that command.")
+"""
